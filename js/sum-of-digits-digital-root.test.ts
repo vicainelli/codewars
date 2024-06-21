c9481f41129d6edf76ada43343895e0bd4fdec51
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2024-06-21 13:04:54 +0200

    feat: add more tests for digitalRoot

--- js/sum-of-digits-digital-root.test.ts
+++ js/sum-of-digits-digital-root.test.ts
@@ -23,4 +23,22 @@ describe("digitalRoot", () => {
   it("return 2 for 493193", () => {
     expect(digitalRoot(493193)).toBe(2);
   });
+  it("return 2 for 992", () => {
+    expect(digitalRoot(992)).toBe(2);
+  });
+  it("return 9 for 999999999999", () => {
+    expect(digitalRoot(999999999999)).toBe(9);
+  });
+  it("return 9 for 167346", () => {
+    expect(digitalRoot(167346)).toBe(9);
+  });
+  it("return 1 for 10", () => {
+    expect(digitalRoot(10)).toBe(1);
+  });
+  it("return 0 for 0", () => {
+    expect(digitalRoot(0)).toBe(0);
+  });
+  it("return 2 for 200", () => {
+    expect(digitalRoot(200)).toBe(2);
+  });
 });
