26cde7347c88151332a660bc4c502513ea2add0f
Vinicius Cainelli <vinicius.cainelli@ingka.ikea.com>
2024-06-21 12:43:53 +0200

    chore: move alphabetPosition to ts

--- js/replace-with-alphabet-position.test.ts
+++ js/replace-with-alphabet-position.test.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from "bun:test";
 
-function alphabetPosition(text) {
+function alphabetPosition(text: string): string {
   return [...text]
-    .reduce((acc, curr) => {
+    .reduce((acc: number[], curr: string) => {
       const letterIndex = [..."abcdefghijklmnopqrstuvwxyz"].findIndex(
-        (item) => item === curr.toLowerCase()
+        (item) => item === curr.toLowerCase(),
       );
 
       if (letterIndex >= 0) {
@@ -12,16 +12,17 @@ function alphabetPosition(text) {
       }
       return acc;
     }, [])
+    .map((num) => num.toString())
     .join(" ");
 }
 
 describe("alphabetPosition", () => {
   it("should return", () => {
     expect(alphabetPosition("The sunset sets at twelve o' clock.")).toBe(
-      "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+      "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11",
     );
     expect(alphabetPosition("The narwhal bacons at midnight.")).toBe(
-      "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+      "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20",
     );
   });
 });
